7564f2ae0225a7f53b8ce8a3110ac1bc572827c3
KikoUguina <kipuguina@gmail.com>
2024-02-14 17:16:19 +0100

    In proccess la Ela

--- src/components/laela/Ela.jsx
+++ src/components/laela/Ela.jsx
@@ -15,7 +15,7 @@ export function Ela({ config }){
 
     return(
         <>
-            <section id='introContainer'>
+            <section id='intro-container'>
                 <h1>La Enfermedad del <span className='elaGreen'>ELA</span></h1>
                 <p>
                     La esclerosis lateral amiotrófica (<span className='elaGreen'>ELA</span>) es una enfermedad degenerativa que afecta a las células nerviosas encargadas del control muscular. A medida que estas células se van dañando y muriendo, los músculos del cuerpo se debilitan y atrofian, lo que puede llevar a problemas de movilidad, habla, respiración y alimentación.
@@ -25,16 +25,19 @@ export function Ela({ config }){
                 </p>
             </section>
 
-            <section id='elaSymptoms'>
+            <section id='ela-symptoms'>
                 <h2>¿Cuáles son los síntomas del <span className='elaGreen'>ELA</span>?</h2>
                 <p>
                     Los síntomas incluyen debilidad muscular, dificultad para hablar y tragar, problemas respiratorios, calambres musculares y espasmos en brazos, hombros y lengua, llanto, risa o bostezos inapropiados y cambios cognitivos y de comportamiento.
                 </p>
-                <div>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/tFUWFustQ5w?si=G4VbFNjUCTffHsyM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-
+                <div id='video-container'>
+                    <iframe src="https://www.youtube.com/embed/tFUWFustQ5w?si=YeikcxSMIjC1dVM9" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum repellendus perspiciatis dignissimos pariatur modi laudantium quo, illo harum vitae consectetur sequi aspernatur repudiandae laboriosam voluptatem, quas cum voluptas accusamus cumque mollitia nemo tenetur facere, culpa sapiente. Ea aperiam aspernatur obcaecati ab architecto quas repellendus, in veniam soluta nostrum, eius illo praesentium ad explicabo asperiores odio ut assumenda, itaque consectetur sunt dolorum. Officia, incidunt praesentium! Nulla et nemo reprehenderit fugiat excepturi incidunt nobis provident architecto. Quae dolorum exercitationem iste accusantium quos.</p>
                 </div>
-                
+            </section>
+
+            <section id='ela-origin'>
+                <h2>¿Cuándo se presenta la <span className='elaGreen'>ELA</span>?</h2>
             </section>
         </>
     )
